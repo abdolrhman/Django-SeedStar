02448513bb8495dc7ac1532698c1bf0f66cc40a8
Nasr <nasr@localhost.localdomain>
2018-09-20 01:23:52 +0200

    edit urls names

--- user_app/urls.py
+++ user_app/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from user_app import views
 
 urlpatterns = [
-    url(r'^$', views.users, name='users'),
-    url(r'^add_user', views.add_user, name='add_user'),
+    url(r'^list', views.users, name='users'),
+    url(r'^add', views.add_user, name='add_user'),
 ]
